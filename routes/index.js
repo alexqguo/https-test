3b573839a1d6977c47da3b826a91696db7458732
Alex Guo <alexguo@amazon.com>
2017-05-11 12:41:23 -0400

    Add xss pages

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,23 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
 
+// X-Frame-Options
 router.get('/xframeoptions', (req, res, next) => {
     res.render('xframeoptions', { title: 'XFRAMEOPTIONS' });
+});
+
+// XSS
+router.get('/xss', (req, res) => {
+    res.append('X-XSS-Protection', '0');
+    res.render('xss', { title: 'XSS', val: req.query.val });
+});
+router.get('/xssprotection', (req, res) => {
+    res.append('X-XSS-Protection', '1');
+    res.render('xss', { title: 'XSS PROTECTION', val: req.query.val });
+});
+router.get('/xssblock', (req, res) => {
+    res.append('X-XSS-Protection', '1;mode=block');
+    res.render('xss', { title: 'XSS PROTECTION', val: req.query.val });
 });
 
 module.exports = router;

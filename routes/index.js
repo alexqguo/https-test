6f78c996a93c9bfee3ba3f86fcf44726c3bf0db0
Alex Guo <alexguo@amazon.com>
2017-05-18 11:49:34 -0400

    Updates

--- routes/index.js
+++ routes/index.js
@@ -15,26 +15,30 @@ router.get('/xframeoptions', (req, res, next) => {
 router.get('/cachecontrol', (req, res) => {
     let isCacheControl = req.query.enabled === 'true';
     let num = Number.parseInt(Math.random() * 1000);
+    let header;
 
     if (isCacheControl) {
-        res.append('Cache-Control', 'no-cache, no-store, must-revalidate');
+        header = 'no-cache, no-store, must-revalidate';
+        res.append('Cache-Control', header);
+    } else {
+        header = 'nothing!!!';
     }
 
-    res.render('cache', { title: 'CACHE', num: num });
+    res.render('cache', { title: 'CACHE', num: num, header: header });
 });
 
 // XSS
 router.get('/xss', (req, res) => {
     res.append('X-XSS-Protection', '0');
-    res.render('xss', { title: 'XSS', val: req.query.val });
+    res.render('xss', { title: 'XSS', val: req.query.val, header: 'X-XSS-Protection: 0' });
 });
 router.get('/xssprotection', (req, res) => {
     res.append('X-XSS-Protection', '1');
-    res.render('xss', { title: 'XSS PROTECTION', val: req.query.val });
+    res.render('xss', { title: 'XSS PROTECTION', val: req.query.val, header: 'X-XSS-Protection: 1' });
 });
 router.get('/xssblock', (req, res) => {
     res.append('X-XSS-Protection', '1;mode=block');
-    res.render('xss', { title: 'XSS PROTECTION', val: req.query.val });
+    res.render('xss', { title: 'XSS PROTECTION', val: req.query.val, header: 'X-XSS-Protection: 1;mode=block' });
 });
 
 // Preconnect
